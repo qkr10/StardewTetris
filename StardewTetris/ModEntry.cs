27b1cb8c0019b03f66f82f666f399a4d1df4defe
qkr10 <qkrtngks10@naver.com>
2020-02-06 22:04:41 +0900

    테트리스 게임 시작메커니즘 추가중 (현재 세미콜론으로 시작) (돌을 T자로 설치해 시작시킬 예정)

--- StardewTetris/ModEntry.cs
+++ StardewTetris/ModEntry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using StardewModdingAPI;
@@ -8,57 +9,128 @@ using StardewValley;
 
 namespace StardewTetris
 {
+	using StardewValleyObject = StardewValley.Object;
+
 	public class ModEntry : Mod
 	{
 		static int keyDownCount = 0;
-		static bool[] keyStates = new bool[256];
+		static public bool[] keyStates = new bool[256];
+		static StardewValleyObject brick = null;
+		static StardewValleyObject empty = null;
+		static TetrisGame tetris;
+		static GameLocation farm;
+		static int boardX = 50, boardY = 20;
 		bool isTetrisRunning = false;
-		//TetrisGame tetris;
+
+		Dictionary<string, int> keyValues = new Dictionary<string, int> {
+			{ "Left", 200 },
+			{ "Right", 201 },
+			{ "Up", 202 },
+			{ "Down", 203 },
+			{ "C", 204 },
+			{ "Z", 205 },
+			{ "X", 206 },
+			{ "Space", 207 }
+		};
 
 		public override void Entry(IModHelper helper)
 		{
 			Array.Clear(keyStates, 0, 256);
+			helper.Events.GameLoop.SaveLoaded += SaveLoaded;
 			helper.Events.Input.ButtonPressed += OnButtonPressed;
 			helper.Events.Input.ButtonReleased += OnButtonReleased;
 			helper.Events.GameLoop.UpdateTicked += Update;
-			//tetris.PutEmpty = PutEmpty;
-			//tetris.PutBrick = PutBrick;
-			//tetris.IsKeyDown = IsKeyDown;
-			//tetris.GetKey = GetKey;
+			helper.Events.World.ObjectListChanged += OnObjectListChanged;
+			tetris = new TetrisGame(boardX, boardY, PutEmpty, PutBrick, IsKeyDown, GetKey, GameOver);
 		}
 
 		static void PutEmpty(int x, int y)
 		{
-			var brick = new StardewValley.Object(75, 1, false, -1, 0);
-			Game1.getLocationFromName("Farm").dropObject(brick, new Vector2(x, y) * 64f, Game1.viewport, true, (Farmer)null);
+			if (empty == null) {
+				Game1.getLocationFromName("farm").removeEverythingFromThisTile(x, y);
+				empty = Game1.getLocationFromName("farm").getObjectAt(x, y);
+				return;
+			}
+			if (empty == Game1.getLocationFromName("farm").getObjectAt(x, y)) {
+				return;
+			}
+			Game1.getLocationFromName("farm").setObjectAt(x, y, empty);
 		}
-		static void PutBrick(int x, int y)
+
+		static void PutBrick(int x, int y, int color)
 		{
-			var brick = new StardewValley.Object(75, 1, false, -1, 0);
-			Game1.getLocationFromName("Farm").dropObject(brick, new Vector2(x, y) * 64f, Game1.viewport, true, (Farmer)null);
+			if (brick == Game1.getLocationFromName("farm").getObjectAt(x, y))
+				return;
+			Game1.getLocationFromName("Farm").setObjectAt(x, y, brick);
 		}
+
 		static bool IsKeyDown()
 		{
 			return keyDownCount != 0;
 		}
+
 		static int GetKey()
 		{
-			for (int i = 0; i < 256; i++) {
-				if (keyStates[i]) {
+			for (int i = 0; i < 256; i++)
+				if (keyStates[i])
 					return i;
+			return -1;
+		}
+
+		static void GameOver()
+		{
+			ResetBoard();
+			tetris.Stop();
+			return;
+		}
+
+		static private void ResetBoard()
+		{
+			for (int y = boardY; y < boardY + 22; y++) {
+				for (int x = boardX; x < boardX + 12; x++) {
+					PutEmpty(x, y);
+					Game1.getLocationFromName("farm").removeTile(x, y, "Front");
 				}
 			}
-			return -1;
+			return;
+		}
+
+		private void InitBoard()
+		{
+			if (brick == null) {
+				brick = new StardewValleyObject(75, 1, false, -1, 0);
+			}
+
+			for (int y = boardY; y < boardY + 22; y++) {
+				for (int x = boardX; x < boardX + 12; x++) {
+					PutEmpty(x, y);
+				}
+				//Monitor.Log($"y좌표 : {y} 타일 인덱스 합 : {sum}", LogLevel.Debug);
+			}
+			Game1.getLocationFromName("farm").setMapTileIndex(boardX, boardY + 21, 0, "Front");
+			Game1.getLocationFromName("farm").setMapTileIndex(boardX, boardY, 1, "Front");
+			Game1.getLocationFromName("farm").setMapTileIndex(boardX + 11, boardY, 2, "Front");
+			Game1.getLocationFromName("farm").setMapTileIndex(boardX + 11, boardY + 21, 3, "Front");
+			for (int x = boardX + 1; x < boardX + 11; x++)
+				Game1.getLocationFromName("farm").setMapTileIndex(x, boardY, 5, "Front");
+			for (int x = boardX + 1; x < boardX + 11; x++)
+				Game1.getLocationFromName("farm").setMapTileIndex(x, boardY + 21, 5, "Front");
+			for (int y = boardY + 1; y < boardY + 21; y++)
+				Game1.getLocationFromName("farm").setMapTileIndex(boardX, y, 6, "Front");
+			for (int y = boardY + 1; y < boardY + 21; y++)
+				Game1.getLocationFromName("farm").setMapTileIndex(boardX + 11, y, 6, "Front");
 		}
 
 		private void Update(object sender, UpdateTickedEventArgs e)
 		{
 			if (!Context.IsWorldReady)
 				return;
+		}
 
-			if (!isTetrisRunning)
+		private void SaveLoaded(object sender, SaveLoadedEventArgs e)
+		{
+			if (!Context.IsWorldReady)
 				return;
-			//tetris.main();
 		}
 
 		private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
@@ -67,15 +139,22 @@ namespace StardewTetris
 				return;
 
 			e.Button.TryGetKeyboard(out Keys key);
-			if (key.ToString() == "OemSemicolon") {
+			string keyString = key.ToString();
+			if (keyString == "OemSemicolon") {
 				isTetrisRunning = !isTetrisRunning;
-				for (int x = 60; x < 75; x++)
-					for (int y = 20; y < 40; y++) {
-						var brick = new StardewValley.Object(75, 1, false, -1, 0);
-						Game1.getLocationFromName("Farm").dropObject(brick, new Vector2(x, y) * 64f, Game1.viewport, true, (Farmer)null);
-					}
+				if (isTetrisRunning) {
+					InitBoard();
+					tetris.Start();
+				}
+				else {
+					ResetBoard();
+					tetris.Stop();
+				}
 			}
-			keyStates[(int)key] = true;
+			
+			if (!keyValues.ContainsKey(keyString))
+				return;
+			keyStates[keyValues[keyString]] = true;
 			keyDownCount++;
 		}
 
@@ -85,8 +164,39 @@ namespace StardewTetris
 				return;
 
 			e.Button.TryGetKeyboard(out Keys key);
-			keyStates[(int)key] = false;
+			string keyString = key.ToString();
+
+			if (!keyValues.ContainsKey(keyString))
+				return;
+			keyStates[keyValues[keyString]] = false;
 			keyDownCount--;
 		}
+
+		static readonly int[,] Tshape = new int[2, 3] { { 1, 1, 1 }, { 0, 1, 0 } };
+		static readonly int[][] Tindex = new int[4][] {
+			new int[2] {0, 0},
+			new int[2] {0, 1},
+			new int[2] {0, 2},
+			new int[2] {1, 1} };
+		static readonly int[] stoneItemCodes = { 2, 4, 75, 76, 77, 290, 343, 390, 411, 449, 450, 751, 760, 762 };
+
+		//private void OnObjectListChanged(object sender, ObjectListChangedEventArgs e)
+		//{
+		//	if (Game1.currentLocation.name != "farm")
+		//		return;
+
+		//	//추가된 오브젝트가 돌일때, T모양으로 설치되어 있으면 T의 가운데 돌 기준으로 테트리스 시작
+		//	foreach (var pair in e.Added) {
+		//		Vector2 cor = pair.Key;
+		//		StardewValleyObject obj = pair.Value;
+		//		if (Array.Exists(stoneItemCodes, O => O == obj.parentSheetIndex)) {
+		//			foreach (int[] ind in Tindex)
+		//				for (int x = 0; x < 3; x++)
+		//					for (int y = 0; y < 2; y++) {
+		//						Game1.getLocationFromName("farm").
+		//					}
+		//		}
+		//	}
+		//}
 	}
 }
--- StardewTetris/TetrisGame.cs
+++ StardewTetris/TetrisGame.cs
@@ -5,26 +5,21 @@ using System.Threading;
 namespace StardewTetris
 {
 	public delegate void PutEmptyDel(int x, int y);
-	public delegate void PutBrickDel(int x, int y);
+	public delegate void PutBrickDel(int x, int y, int color);
 	public delegate bool IsKeyDownDel();
 	public delegate int GetKeyDel();
-	public delegate int MoveCursorDel();
+	public delegate void GameOverDel();
 
 	class TetrisGame
 	{
-		const int LEFT = 75;
-		const int RIGHT = 77;
-		const int UP = 72;
-		const int DOWN = 80;
-		const int ESC = 27;
-		const int BX = 5;
-		const int BY = 1;
-		const int BW = 10;
-		const int BH = 20;
-		const int PGUP = 73;
-		const int PGDN = 81;
-		const int HOLD = 104;
-		const int DO_SUFFLE = -1;
+		const int Left = 200;
+		const int Right = 201;
+		const int Up = 202;
+		const int Down = 203;
+		const int Hold = 204;
+		const int LeftRot = 205;
+		const int RightRot = 206;
+		const int Space = 207;
 
 		int[,,] Shape = new int[7, 4, 8]{
 			{ {0,0,1,0,2,0,-1,0}, {0,0,0,1,0,-1,0,-2}, {0,0,1,0,2,0,-1,0}, {0,0,0,1,0,-1,0,-2} },
@@ -34,11 +29,11 @@ namespace StardewTetris
 			{ {0,0,-1,0,1,0,-1,-1}, {0,0,0,-1,0,1,-1,1}, {0,0,-1,0,1,0,1,1}, {0,0,0,-1,0,1,1,-1} },
 			{ {0,0,1,0,-1,0,1,-1}, {0,0,0,1,0,-1,-1,-1}, {0,0,1,0,-1,0,-1,1}, {0,0,0,-1,0,1,1,1} },
 			{ {0,0,-1,0,1,0,0,1}, {0,0,0,-1,0,1,1,0}, {0,0,-1,0,1,0,0,-1}, {0,0,-1,0,0,-1,0,1} },
-		};         
+		};
 		//구조체 3차원 배열으로 벽돌모양을 표현한다
 		//Shape[벽돌모양, 벽돌의회전, 좌표값]
-		
-		enum CellStates{ EMPTY, BRICK, WALL };
+
+		enum CellStates { EMPTY, BRICK, WALL };
 
 		int[,] spinCenter = new int[10, 2]{
 			{0, 0},{1, 0},{-1, 0},
@@ -47,42 +42,43 @@ namespace StardewTetris
 			{0, -1}
 		};
 
-		int[] box = new int[]{ 0, 1, 2, 3, 4, 5, 6 };
+		int[] box = new int[] { 0, 1, 2, 3, 4, 5, 6 };
 
-		int[,] board = new int[BW + 2,BH + 2];
+		int boardX = 50;
+		int boardY = 20;
+		const int boardW = 10;
+		const int boardH = 20;
+
+		int[,] board = new int[boardW + 2, boardH + 2];
 		int nx, ny;
 		int brick, rot;
-		int nbrick;
-		int bricknum;
+		int holdTrig;
+		int nbrick = 6;
 		int hbrick = 8;
-		int HoldTrig = 1;
-		int DropTime = 1000000000;
+		long DropTime = 0;
 
-		static void PutEmptyVir(int x, int y)
-		{
-			throw new NotImplementedException();
-		}
-		static void PutBrickVir(int x, int y)
-		{
-			throw new NotImplementedException();
-		}
-		static bool IsKeyDownVir()
-		{
-			throw new NotImplementedException();
-		}
-		static int GetKeyVir()
-		{
-			throw new NotImplementedException();
-		}
-
-		public PutEmptyDel PutEmpty = PutEmptyVir;
-		public PutBrickDel PutBrick = PutBrickVir;
-		public IsKeyDownDel IsKeyDown = IsKeyDownVir;
-		public GetKeyDel GetKey = GetKeyVir;
+		public PutEmptyDel PutEmpty;
+		public PutBrickDel PutBrick;
+		public IsKeyDownDel IsKeyDown;
+		public GetKeyDel GetKey;
+		public GameOverDel GameOver;
 
 		Random rand = new Random();
 
 		Thread thread;
+
+		public TetrisGame(int boardX, int boardY, 
+			PutEmptyDel putEmpty, PutBrickDel putBrick,
+			IsKeyDownDel isKeyDown, GetKeyDel getKey, GameOverDel gameOver)
+		{
+			this.boardX = boardX;
+			this.boardY = boardY;
+			PutEmpty = putEmpty;
+			PutBrick = putBrick;
+			IsKeyDown = isKeyDown;
+			GetKey = getKey;
+			GameOver = gameOver;
+		}
 
 		public void Start()
 		{
@@ -90,67 +86,63 @@ namespace StardewTetris
 			thread.Start();
 		}
 
+		public void Stop()
+		{
+			thread.Abort();
+		}
+
 		private void Running()
 		{
-			int nFrame, nStay;
 			int x, y;
-			while (true) {
-				for (x = 0; x < BW + 2; x++)
-					for (y = 0; y < BH + 2; y++)
-						board[x, y] = (int)((y == 0 || y == BH + 1 || x == 0 || x == BW + 1) ? CellStates.WALL : CellStates.EMPTY);
-				//board 배열에 WALL이나 EMPTY를 넣음
-				//BW 와 BH는 벽돌이 실제 움직이는 공간이므로 +2씩 한다
-				nFrame = 20;
-				bricknum = 0;
-
-				Shuffle();
+			for (x = 0; x < boardW + 2; x++)
+				for (y = 0; y < boardH + 2; y++)
+					board[x, y] = (int)((y == 0 || y == boardH + 1 || x == 0 || x == boardW + 1) ? CellStates.WALL : CellStates.EMPTY);
+			
+			nbrick = GetNextBrick(nbrick);
+			while (true) {//무한히 벽돌을 생성한다
+				brick = nbrick;
 				nbrick = GetNextBrick(nbrick);
-				while (true) {
-					bricknum++;
-					brick = nbrick;
-					nbrick = GetNextBrick(nbrick);
+				holdTrig = 1;
 
-					nx = BW / 2;      //nx,ny는 떨어지고있는 벽돌의 좌표값
-					ny = 3;
-					rot = 0;
-					PrintBri();
+				nx = boardW / 2;
+				ny = 3;
+				rot = 0;
+				PrintBri();
 
-					if (GetAround(nx, ny, brick, rot) != (int)CellStates.EMPTY) break;
-					nStay = nFrame;
-					while (true) {
-						if (--nStay == 0) {
-							nStay = nFrame;
-							if (MoveDown()) break;
-						}
-						if (ProcessKey()) break;
-						delay(1000 / 20);
+				if (GetAround(nx, ny, brick, rot) != (int)CellStates.EMPTY)
+					break;//벽돌 생성이 안되면 게임 종료
+				DropTime = clock();
+
+				long buttonTime = clock();
+				long gravityTime = clock();
+				while (true) {//벽돌이 하강
+					if (gravityTime + 500 < clock()) {
+						gravityTime = clock();
+						if (MoveDown())
+							break;
 					}
-					if (bricknum % 10 == 0 && nFrame > 5) {
-						nFrame--;
+					if (buttonTime + 100 < clock()) {
+						buttonTime = clock();
+						if (ProcessKey())
+							break;
 					}
 				}
 			}
 		}
 
-		private void delay(int v)
-		{
-			int old = clock();
-			while (old + v < clock()) ;
-		}
-
 		private void PrintBri()
 		{
 			for (int i = 0; i < 4; i++) {
-				int xCor = BX + (Shape[brick, rot, i * 2] + nx) * 2;
-				int yCor = BY + Shape[brick, rot, i * 2 + 1] + ny;
-				PutBrick(xCor, yCor);
+				int xCor = boardX + Shape[brick, rot, i * 2] + nx;
+				int yCor = boardY + Shape[brick, rot, i * 2 + 1] + ny;
+				PutBrick(xCor, yCor, brick);
 			}
 		}
 		private void EraseBri()
 		{
 			for (int i = 0; i < 4; i++) {
-				int xCor = BX + (Shape[brick, rot, i * 2] + nx) * 2;
-				int yCor = BY + Shape[brick, rot, i * 2 + 1] + ny;
+				int xCor = boardX + Shape[brick, rot, i * 2] + nx;
+				int yCor = boardY + Shape[brick, rot, i * 2 + 1] + ny;
 				PutEmpty(xCor, yCor);
 			}
 		}
@@ -164,7 +156,7 @@ namespace StardewTetris
 			if (IsKeyDown()) {
 				ch = GetKey();
 				switch (ch) {
-					case LEFT:
+					case Left:
 						if (GetAround(nx - 1, ny, brick, rot) == (int)CellStates.EMPTY) {
 							EraseBri();
 							nx--;
@@ -172,7 +164,7 @@ namespace StardewTetris
 							DropTime = clock();
 						}
 						break;
-					case RIGHT:
+					case Right:
 						if (GetAround(nx + 1, ny, brick, rot) == (int)CellStates.EMPTY) {
 							EraseBri();
 							nx++;
@@ -180,28 +172,18 @@ namespace StardewTetris
 							DropTime = clock();
 						}
 						break;
-					case UP:
-						trot = (rot == 3 ? 0 : rot + 1);
-						if (GetAround(nx, ny, brick, trot) == (int)CellStates.EMPTY) {
-							EraseBri();
-							rot = trot;
-							PrintBri();
-							DropTime = clock();
-						}
-						break;
-					case DOWN:
+					case Down:
 						ret = MoveDown();
 						break;
-					case HOLD:
-						HoldBrick();
-						break;
-					case 'c':          //c입력시 블럭 홀드
-						if (HoldTrig == 0)
+					case Hold:          //c입력시 블럭 홀드
+						if (holdTrig == 0)
 							break;
+						holdTrig = 0;
 						HoldBrick();
 						PrintBri();
 						break;
-					case 'z':
+					case Up:
+					case LeftRot:
 						trot = (rot == 3 ? 0 : rot + 1);
 						if (GetAroundSpin(nx, ny, brick, trot, ref xx, ref yy) == (int)CellStates.EMPTY) {
 							EraseBri();
@@ -212,7 +194,7 @@ namespace StardewTetris
 							DropTime = clock();
 						}
 						break;
-					case 'x':
+					case RightRot:
 						trot = (rot == 0 ? 3 : rot - 1);
 						if (GetAroundSpin(nx, ny, brick, trot, ref xx, ref yy) == (int)CellStates.EMPTY) {
 							EraseBri();
@@ -223,7 +205,7 @@ namespace StardewTetris
 							DropTime = clock();
 						}
 						break;
-					case ' ':
+					case Space:
 						while (MoveDown() == false) {; }
 						return true;
 				}
@@ -235,7 +217,11 @@ namespace StardewTetris
 		{                                       //이동중인 벽돌의 주변을 조사하는 것이 아니므로 인수로 전달된 위치의 벽돌모양을 참조한다
 			int k = (int)CellStates.EMPTY;
 			for (int i = 0; i < 4; i++) {
-				k = Math.Max(k, board[x + Shape[b,r,i*2], y + Shape[b,r,i*2+1]]);
+				int xx = x + Shape[b, r, i * 2];
+				int yy = y + Shape[b, r, i * 2 + 1];
+				if (xx > boardW + 1 || xx < 0 || yy > boardY + 1 || yy < 0)
+					continue;
+				k = Math.Max(k, board[xx, yy]);
 			}
 			return k;
 		}
@@ -247,8 +233,13 @@ namespace StardewTetris
 				int k = (int)CellStates.EMPTY;
 				int xx = spinCenter[j,0] + x;
 				int yy = spinCenter[j,1] + y;
-				for (i = 0; i < 4; i++)
-					k = Math.Max(k, board[xx + Shape[b,r,i*2],yy + Shape[b,r,i*2+1]]);
+				for (i = 0; i < 4; i++) {
+					int xxx = xx + Shape[b, r, i * 2];
+					int yyy = yy + Shape[b, r, i * 2 + 1];
+					if (xxx > boardW + 1 || xxx < 0 || yyy > boardY + 1 || yyy < 0)
+						continue;
+					k = Math.Max(k, board[xxx, yyy]);
+				}
 				if (k == (int)CellStates.EMPTY) {
 					retx = xx;
 					rety = yy;
@@ -261,9 +252,9 @@ namespace StardewTetris
 		bool MoveDown()   //벽돌을 한칸 아래로 이동시킨다.
 		{
 			if (GetAround(nx, ny + 1, brick, rot) != (int)CellStates.EMPTY) {
-				if (DropTime + 500 >= clock())
+				if (DropTime + 500 >= clock()) {
 					return false;
-				HoldTrig = 1;
+				}
 				TestFull();
 				return true;//바닥에 닿았다면 TestFull() 한 후 TRUE를 리턴한다.
 			}
@@ -274,9 +265,9 @@ namespace StardewTetris
 			return false;
 		}
 
-		private int clock()
+		private long clock()
 		{
-			return (int)(DateTime.Now.Ticks / 10000);
+			return DateTime.Now.Ticks / 10000;
 		}
 
 		void TestFull()              //수평으로 다 채워진 줄을 찾아 삭제한다
@@ -289,25 +280,24 @@ namespace StardewTetris
 				board[nx + Shape[brick,rot,i*2],ny + Shape[brick,rot,i*2+1]] = (int)CellStates.BRICK;
 			}
 
-			for (y = 1; y < BH + 1; y++) {
-				for (x = 1; x < BW + 1; x++) {
+			for (y = 1; y < boardH + 1; y++) {
+				for (x = 1; x < boardW + 1; x++) {
 					if (board[x,y] != (int)CellStates.BRICK) break;
 				}
-				if (x == BW + 1) {
+				if (x == boardW + 1) {
 					count++;
 					for (ty = y; ty > 1; ty--) {
-						for (x = 1; x < BW + 1; x++) {
+						for (x = 1; x < boardW + 1; x++) {
 							board[x,ty] = board[x,ty - 1];
 						}
 					}
-					delay(200);
 				}
 			}
 		}
 
 		void HoldBrick()
 		{ //블럭을 홀드한다
-			HoldTrig = 0;
+			EraseBri();
 			if (hbrick == 8) {
 				hbrick = brick;
 				brick = nbrick;
@@ -316,20 +306,13 @@ namespace StardewTetris
 			else {
 				swap(ref brick, ref hbrick);
 			}
-			nx = BW / 2;
+			nx = boardW / 2;
 			ny = 3;
+			rot = 0;
 			for (int i = 0; i < 4; i++) {
-				int xCor = BX + (Shape[brick, rot, i * 2] + nx) * 2;
-				int yCor = BY + Shape[brick, rot, i * 2 + 1] + ny;
+				int xCor = boardX + (Shape[brick, rot, i * 2] + nx) * 2;
+				int yCor = boardY + Shape[brick, rot, i * 2 + 1] + ny;
 				PrintBri();
-			}
-		}
-
-		void Shuffle()
-		{
-			int[] start = box;
-			for (int i = 0; i < 7; i++) {
-				swap(ref start[i], ref start[i + Random(7 - i)]);
 			}
 		}
 
@@ -342,7 +325,7 @@ namespace StardewTetris
 		{
 			int temp = v1;
 			v1 = v2;
-			v2 = v1;
+			v2 = temp;
 		}
 
 		int GetNextBrick(int previousBrick)
@@ -354,6 +337,13 @@ namespace StardewTetris
 			int i = 0;
 			while (box[i++] != previousBrick);
 			return box[i];
+		}
+
+		void Shuffle()
+		{
+			int[] start = box;
+			for (int i = 0; i < 6; i++)
+				swap(ref start[i], ref start[i + Random(6 - i) + 1]);
 		}
 	}
 }
